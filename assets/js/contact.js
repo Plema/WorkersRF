53d2e6a9513e08584ee34a688768fd8a28b1d835
OdrinskayaMargarita <margarita.odrinskaya@gmail.com>
2020-07-23 13:26:34 +0300

    all-services and contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,24 +2,19 @@
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
           center: [59.9114037, 30.3487953],
-          zoom: 14
+          zoom: 13
       }, {
           searchControlProvider: 'yandex#search'
-      }),
-
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: №799370; font-weight: bold;">$[properties.iconContent]</div>'
-      ),
-
-      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-          balloonContent: 'г. Москва, ул. Ленина, 25'
-      }, {
-          iconLayout: 'default#image',
-          iconImageHref: '../img/vector.png',
-          iconImageSize: [30, 42],
-          iconImageOffset: [-5, -38]
-      });
-
-  myMap.geoObjects
-      .add(myPlacemark)
+      })
 });
+
+$(document).ready(function(){
+ 
+    $(".title").bind("click", function(event){
+    //   $('.answer').slideToggle();
+      $(this).parent().toggleClass('open-quest');
+    });
+   
+  
+  });
+  
