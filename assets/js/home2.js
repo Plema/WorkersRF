9e5fb1582dd35335ce0de5c1d14e687d094d7ead
OdrinskayaMargarita <margarita.odrinskaya@gmail.com>
2020-07-24 14:25:37 +0300

    page-vacancy and half of index2

---
+++ assets/js/home2.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+ 
+  
+  $('.slider-banner').slick({
+    dots: true,
+    arrows: true,
+    infinite: false,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+  
+  $('.slider-clients').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    adaptiveWidth: true,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 5,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 2,
+        }
+      }
+    ]
+  });
+
+  
+  //Slick-empty
+  $('.slick-arrow').empty();
+  $('.slick-dots > li > button').empty();
+
+});
